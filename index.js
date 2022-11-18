e1f792549fc94eb01449406dbeecaab556b50a1d
mohamedsek <mohamed.sekkoume@gmail.com>
2022-11-18 15:21:50 +0100

    feature add API/user/

--- index.js
+++ index.js
@@ -156,9 +156,6 @@ app.get('/api/user/profile', async (req, res) => {
     const user = await User.find({ email: usermail })
 
     return res
-        .cookie("token", token, {
-            httpOnly: true
-        })
         .status(200).json({
             email: user.email,
             lastname: user.lastname,
@@ -168,7 +165,7 @@ app.get('/api/user/profile', async (req, res) => {
 
 })
 
-app.put('/api/user/edit', async (req, res) => { 
+app.put('/api/user/edit', async (req, res) => {
 
     const body = req.body
     const token = req.cookies.token
@@ -189,28 +186,201 @@ app.put('/api/user/edit', async (req, res) => {
 
     // verifier le nom et prenom saisie par l'utilisateur
 
-    
+    if (body.firstname.length <= 1)
+        return res.status(401).json({
+            error: true,
+            message: "firstname incorrect"
+        })
 
+    if (body.lastname.length <= 1)
+        return res.status(401).json({
+            error: true,
+            message: "lastname incorrect"
+        })
+    // changer les informations utilisateurs dans la BD
+    filter = { email: usermail }
+    update = { lastname: body.lastname, firstname: body.firstname }
+    user = user.findOneAndUpdate(filter, update, {
+        new: true
+    });
+
+    return res
+        .status(201)
+        .json({
+            email: user.email,
+            lastname: user.lastname,
+            firstname: user.firstname,
+            phone: user.phone,
+            message: "Modification du compte réussie !"
+        })
+
+})
+
+app.put('/api/user/edit-password', async (req, res) => {
+
+    const body = req.body
+    const token = req.cookies.token
+
+    if (!token) {
+        return res.status(403);
+    }
+    // recuperer l'email de l'utilisateur depuis le token
+    try {
+        const data = jwt.verify(token, "mySecret");
+        usermail = data.email;
+    } catch {
+        return res.status(403);
+    }
+
+    const user = await User.find({ email: usermail })
+
+
+    // verifier le password saisie par l'utilisateur
+
+    if (body.password.length <= 16)
+        return res.status(401).json({
+            error: true,
+            message: "password incorrect"
+        })
+
+
+    if (body.password != body.confirmpassword)
+        return res.status(401).json({
+            error: true,
+            message: "password confirmation is incorrect"
+        })
+
+    // 
+    const hashpassword = await argon2.hash(body.password);
+
+
+    // changer les informations utilisateurs dans la BD
+    filter = { email: usermail }
+    update = { password: hashpassword }
+    user = user.findOneAndUpdate(filter, update, {
+        new: true
+    });
+
+    return res.status(201).json({
+        message: "Mot de passe modifié !"
+    })
+
+
+})
+
+app.put('/api/user/edit-phone', async (req, res) => {
+
+    const body = req.body
+    const token = req.cookies.token
+
+    if (!token) {
+        return res.status(403);
+    }
+    // recuperer l'email de l'utilisateur depuis le token
+    try {
+        const data = jwt.verify(token, "mySecret");
+        usermail = data.email;
+    } catch {
+        return res.status(403);
+    }
+
+    const user = await User.find({ email: usermail })
+
+
+    // verifier le telephone saisie par l'utilisateur
+
+    if (body.phone.length <= 1)
+        return res.status(401).json({
+            error: true,
+            message: "phone incorrect"
+        })
+
+    // changer les informations utilisateurs dans la BD
+    filter = { email: usermail }
+    update = { phone: body.phone }
+    user = user.findOneAndUpdate(filter, update, {
+        new: true
+    });
+
+    return res
+        .status(201)
+        .json({
+            message: "Numéro de téléphone modifié !"
+        })
+
+})
+
+app.put('/api/user/edit-email', async (req, res) => {
+
+    const body = req.body
+    const token = req.cookies.token
+
+    if (!token) {
+        return res.status(403);
+    }
+    // recuperer l'email de l'utilisateur depuis le token
+    try {
+        const data = jwt.verify(token, "mySecret");
+        usermail = data.email;
+    } catch {
+        return res.status(403);
+    }
+
+    const user = await User.find({ email: usermail })
+
+
+    // verifier l'email saisie par l'utilisateur
+
+    if (body.email.length <= 1)
+        return res.status(401).json({
+            error: true,
+            message: "email incorrect"
+        })
+
+    // changer les informations utilisateurs dans la BD
+    filter = { email: usermail }
+    update = { email: body.email }
+    user = user.findOneAndUpdate(filter, update, {
+        new: true
+    });
+
+    token = await jwt.sign({ email: user.email }, 'mySecret');
     return res
         .cookie("token", token, {
             httpOnly: true
         })
         .status(200).json({
-            email: user.email,
-            lastname: user.lastname,
-            firstname: user.firstname,
-            phone: user.phone
+            message: "Email modifié !"
+        })
+})
+
+app.delete('/api/user/delete', async (req, res) => {
+
+    const body = req.body
+    const token = req.cookies.token
+
+    if (!token) {
+        return res.status(403);
+    }
+    // recuperer l'email de l'utilisateur depuis le token
+    try {
+        const data = jwt.verify(token, "mySecret");
+        usermail = data.email;
+    } catch {
+        return res.status(403);
+    }
+
+    // chercher et supprimer l'utilisateur de la BD
+    const user = await User.findOneAndDelete({ email: usermail });
+
+    return res
+        .status(200)
+        .clearCookie("token")
+        .json({
+            message: "Votre profil a été supprimé ! Un email de confirmation de suppression vous a été envoyé"
         })
 
 })
-
-app.put('/api/user/edit-password', async (req, res) => { })
-
-app.put('/api/user/edit-phone', async (req, res) => { })
-
-app.put('/api/user/edit-email', async (req, res) => { })
-
-app.delete('/api/user/delete', async (req, res) => { })
 
 const start = async () => {
     try {
